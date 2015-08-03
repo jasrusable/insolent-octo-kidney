73edba083c004345c3e313f0002f3395a81682af
Jason Russell <jasrusable@gmail.com>
2015-08-03 20:21:13 +0200

    Fix cache implementation

--- ephemeris_interpolator/satellites.py
+++ ephemeris_interpolator/satellites.py
@@ -10,14 +10,14 @@ class Satellite(object):
     def __init__(self, id=None):
         self.id = id
         self.observations = []
-        self.poly_func_cache = None
+        self.get_ordinate_polynomial_cache = None
 
     def __repr__(self):
         return 'Satellite(id={id})'.format(id=self.id)
 
     def add_observation(self, x, y, z, time):
         # Invalidate poly_func cache
-        self.poly_func_cache = None
+        self.get_ordinate_polynomial_cache = None
         self.observations.append(
             Observation(x, y, z, time)
         )
@@ -26,22 +26,29 @@ class Satellite(object):
         """ Returns a numpy.poly1d function with 
         y-axis as ordinate, and x-axis as time.
         """
-        if self.poly_func_cache:
-            poly_func = self.poly_func_cache
-        else:
-            x, y = [], []
-            for observation in self.observations:
-                x.append(observation.time)
-                y.append(getattr(observation.coordinate, ordinate))
-            poly_func = numpy.poly1d(numpy.polyfit(x, y, degree))
-        return poly_func
+        x, y = [], []
+        for observation in self.observations:
+            x.append(observation.time)
+            y.append(getattr(observation.coordinate, ordinate))
+        return numpy.poly1d(numpy.polyfit(x, y, degree))
 
-    def get_coordinate_at_time(self, time, polynomial_degree=None):
+    def get_coordinate_at_time(self, time, polynomial_degree):
         """ Returns Coordinate of a satellite at time,
         based on interpolation of observations.
         """
-        if not polynomial_degree:
-            polynomial_degree = len(self.observations) - 1
-        xyz = [self.get_ordinate_polynomial(ordinate, polynomial_degree)(time) 
-            for ordinate in ['x', 'y', 'z']]
-        return Coordinate(x=xyz[0], y=xyz[1], z=xyz[2])
+        if self.get_ordinate_polynomial_cache:
+            if self.get_ordinate_polynomial_cache['polynomial_degree'] != polynomial_degree:
+                self.get_ordinate_polynomial_cache = None
+
+        if not self.get_ordinate_polynomial_cache:
+            self.get_ordinate_polynomial_cache = {
+                'x_polynomial': self.get_ordinate_polynomial('x', polynomial_degree),
+                'y_polynomial': self.get_ordinate_polynomial('y', polynomial_degree),
+                'z_polynomial': self.get_ordinate_polynomial('z', polynomial_degree),
+                'polynomial_degree': polynomial_degree,
+            }
+        x = self.get_ordinate_polynomial_cache['x_polynomial'](time)
+        y = self.get_ordinate_polynomial_cache['y_polynomial'](time)
+        z = self.get_ordinate_polynomial_cache['z_polynomial'](time)
+
+        return Coordinate(x=x, y=y, z=z)
