fa96e5ccef87305bdbb92a5d007f89e484383449
Jason Russell <jasrusable@gmail.com>
2015-08-03 19:28:44 +0200

    Improve exceptions

--- ephemeris_interpolator/sessions.py
+++ ephemeris_interpolator/sessions.py
@@ -7,10 +7,19 @@ class Session(object):
 
     def add_satellite(self, id):
         if self.get_satellite(id):
-            raise Exception("Satellite with id {id} already exists.".format(id=id))
+            raise Exception("Failed to add satellite. Satellite with id: {id} already exists."
+                .format(id=id))
         self.satellites.append(
             Satellite(id)
         )
+
+    def remove_satellite(self, id):
+        if not self.get_satellite(id):
+            raise Exception("Failed to remove satellite. Satellite with id: {id} does not exist."
+                .format(id=id))
+        for satellite in self.satellites:
+            if satellite.id == id:
+                self.satellites.remove(satellite)
 
     def get_satellite(self, id):
         for satellite in self.satellites:
@@ -21,5 +30,6 @@ class Session(object):
     def add_observation(self, satellite_id, x, y, z, time):
         satellite = self.get_satellite(satellite_id)
         if not satellite:
-            raise Exception("Cannot find Satellite with id: {id}".format(id=satellite_id))
+            raise Exception("Failed to add observation. Satellite with id: {id} does not exist."
+                .format(id=satellite_id))
         satellite.add_observation(x, y, z, time)
