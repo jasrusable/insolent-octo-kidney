f72fe4871d8e61b1b876fa67e9639eb0218bb86d
Jason Russell <jasrusable@gmail.com>
2015-08-03 20:21:49 +0200

    Add example for getting ordinate

--- demo.py
+++ demo.py
@@ -14,3 +14,6 @@ session.add_observation('G03', x=3, y=1, z=1, time=2)
 
 # Get a satellite
 my_satellite = session.get_satellite('G03')
+
+# Get x value for satellite Coordinate at specific time
+my_satellite.get_coordinate_at_time(0.5, polynomial_degree=1).x
