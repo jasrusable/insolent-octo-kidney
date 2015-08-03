556077fd30cbe1aef81c83a90cb7e9b8f6977a59
Jason Russell <jasrusable@gmail.com>
2015-08-03 19:17:03 +0200

    Add comment for getting a Satellite

--- demo.py
+++ demo.py
@@ -12,5 +12,5 @@ session.add_observation('G03', x=1, y=1, z=1, time=0)
 session.add_observation('G03', x=2, y=1, z=1, time=1)
 session.add_observation('G03', x=3, y=1, z=1, time=2)
 
-
+# Get a satellite
 my_satellite = session.get_satellite('G03')
